e64bfd5041e1a2f51d1cd327e6b38974f8f6fdce
GwenLoezic <loezicgwenael@gmail.com>
2021-03-22 15:57:14 +0100

    Tesla

---
+++ Components/CarItem/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {View, Text, ImageBackground} from 'react-native';
+import StyledButton from '../StyledButton/index.js';
+import styles from './style';
+
+const CarItem = (props) => {
+const { name, tagline, taglineCTA, image } = props.car;
+
+    return (
+        <View style={styles.carContainer}>
+        <ImageBackground 
+          source={image}
+          style={styles.image}/>
+  
+          <View style={styles.titles}>
+  
+            <Text style={styles.title}>{name}</Text>
+            <Text style={styles.subtitle}>
+            {tagline}
+            {' '}
+            <Text style={styles.subtitleCTA}>
+            {taglineCTA}
+            </Text>
+            </Text>
+  
+          </View>
+
+
+          <View style={styles.buttonsContainer}>
+
+          <StyledButton type="primary"
+            content={"Custom Order"}
+            onPress={()=> {console.warn("Custom order was press !");
+          }}
+          />
+
+          <StyledButton type="secondary"
+          content={"Existing Inventory"}
+          onPress={()=> {console.warn("Existing Inventory was press !");
+        }}
+        />
+          
+          
+          </View>
+          
+          
+        </View>
+    );
+};
+
+export default CarItem;
